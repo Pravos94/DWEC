41e9184a544fe4c9ce51cddb5e0443bc541e448f
MAngelo22 <45183250+MAngelo22@users.noreply.github.com>
2023-10-23 16:25:52 +0200

    Añado al DOM las primeras casillas de texto @Miguel

--- AE-1. DOM y Formularios/js/arbol_DOM.js
+++ AE-1. DOM y Formularios/js/arbol_DOM.js
@@ -20,7 +20,7 @@ function cargarDOM(){
     /// --- Fin H3 --- ///
     
     var element_p = document.createElement('p');
-    var tagP_content = document.createTextNode('Debe rellenar el siguiente formulario para crear su perfil');    
+    var tagP_content = document.createTextNode('Debe rellenar el siguiente formulario para crear su perfil: ');    
     element_p.appendChild(tagP_content);
 
     document.body.appendChild(element_p); // Párrafo creado y vinculado
@@ -33,6 +33,60 @@ function cargarDOM(){
     // atribForm.appendChild(document.createTextNode('perfilUser'));
 
     // formulario.appendChild();
+
+    /*Intento de Creadion de texto "Nombre: "*/
+    var SaltoLinea = document.createElement('br');
+    
+
+    var TypeText = document.createElement('type' , Text);
+    var TypeNumber = document.createElement('type', Number);
+    var newNombre = document.createElement('input', TypeText, id="fNombre");    
+    var newApellido = document.createElement('input', TypeText, id="fApellido");
+    var newDNI = document.createElement('input', TypeText, id="fDNI");
+    var newDireccion = document.createElement('input', TypeText, id="fDireccion");
+    var newTelef = document.createElement('input', TypeNumber, id="fTelef");
+    var tagContent_Nombre = document.createTextNode('Nombre: ');
+    var tagContent_Apellido = document.createTextNode('Apellidos: ');
+    var tagContent_DNI = document.createTextNode('DNI/NIE: ');
+    var tagContent_Direccion = document.createTextNode('Dirección: ');
+    var tagContent_Telef = document.createTextNode('Teléfono: ');
+    
+
+    document.body.appendChild(tagContent_Nombre);
+    document.body.appendChild(newNombre);
+    document.body.appendChild(SaltoLinea.cloneNode(true));
+    document.body.appendChild(SaltoLinea.cloneNode(true));
+    console.log("se ha generado casilla Nombre")
+
+    document.body.appendChild(tagContent_Apellido);
+    document.body.appendChild(newApellido);
+    document.body.appendChild(SaltoLinea.cloneNode(true));
+    document.body.appendChild(SaltoLinea.cloneNode(true));
+    console.log("se ha generado casilla Apellido")
+
+    document.body.appendChild(tagContent_DNI);
+    document.body.appendChild(newDNI);
+    document.body.appendChild(SaltoLinea.cloneNode(true));
+    document.body.appendChild(SaltoLinea.cloneNode(true));
+    console.log("se ha generado casilla DNI")
+
+    document.body.appendChild(tagContent_Direccion);
+    document.body.appendChild(newDireccion);
+    document.body.appendChild(SaltoLinea.cloneNode(true));
+    document.body.appendChild(SaltoLinea.cloneNode(true));
+    console.log("se ha generado casilla Direccion")
+
+    document.body.appendChild(tagContent_Telef);
+    document.body.appendChild(newTelef);
+    document.body.appendChild(SaltoLinea.cloneNode(true));
+    document.body.appendChild(SaltoLinea.cloneNode(true));
+    console.log("se ha generado casilla Telefono");
+
+    /*Fin de Cuadros de texto*/
+
+    
+
+    
 
 }
 
