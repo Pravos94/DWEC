c465f433eba3b24a63aabf58ceb65b93a1be0f47
MAngelo22 <45183250+MAngelo22@users.noreply.github.com>
2023-10-30 23:28:23 +0100

    Subo ultimo intento

--- AE-1. DOM y Formularios/js/arbol_DOM_R2.js
+++ AE-1. DOM y Formularios/js/arbol_DOM_R2.js
@@ -191,17 +191,17 @@ function cargarDOM_R2(){
         var btnSubmit = document.createElement('input'); 
         btnSubmit.setAttribute("type", "submit");
         btnSubmit.setAttribute("value", "Procesar Pedido");
-        //btnSubmit.onclick(calcularPrecio);
-        btnSubmit.setAttribute("onclick", calcularPrecio);
+        btnSubmit.onclick=calcularPrecio;
+        //btnSubmit.setAttribute("onclick", calcularPrecio);
+        console.log ("se ha generado boton");
 
         fieldset.appendChild(btnSubmit);
-
-        /*
-        function calcularPrecio(){
+        
+        function calcularPrecio(elDiv){
             
             var precio;
-    
-            If (document.getElementById('Ingrediente_Bacon').checked); {
+    /*
+            If (document.getElementById(newCheckBox1.getAttribute, id ="Ingrediente_Bacon").checked); {
                 precio = precio +1}
                 
             If (document.getElementById("Ingrediente_Peperoni").checked); {
@@ -218,14 +218,14 @@ function cargarDOM_R2(){
                 
             If (document.getElementById("TamGra").checked); {
                 precio = precio +15}
-
+*/
             var Contenido_Factura = document.createTextNode('Has Comprado una pizza por '+ precio +' €');
             document.appendChild(SaltoLinea.cloneNode(true));
             document.appendChild(SaltoLinea.cloneNode(true));
             document.appendChild(SaltoLinea.cloneNode(true));
 
             document.appendChild(Contenido_Factura);
-
+            alert (Contenido_Factura);
             console.log('Se realiza Compra');
             console.log(Contenido_Factura);
             console.log(precio);
@@ -237,7 +237,7 @@ function cargarDOM_R2(){
             alert("Pedido REalizado! " + Contenido_Factura );
             elDiv.addEventListener("click", muestraMensaje);
         //------------------------------------------------
-        }*/
+        }
 
 }
 
