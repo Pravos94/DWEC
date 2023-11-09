8e32947b88c267f3812cd953184f1d5784f17c8b
Pravos94 <alvaropr69@gmail.com>
2023-11-09 23:44:18 +0100

    fix CargaDatosJson

--- AE-2_AJAX/js/pizzeria.js
+++ AE-2_AJAX/js/pizzeria.js
@@ -2,14 +2,15 @@
 const URL_DESTINO = "http://localhost:5500/AE-2_AJAX/"
 const RECURSO = "datosPizzeria.json"
 
-window.onload = function () {
+window.onload = function () { // usamos este evento que se desencadena tras pintarse todo el html
 
     cargarDatosJson()
 
-    function cargarDatosJson() {
+    function cargarDatosJson() { 
 
-        let xmlHttp = new XMLHttpRequest()
+        let xmlHttp = new XMLHttpRequest() // creamos la conexión HTTP
 
+        xmlHttp.open('GET', URL_DESTINO + RECURSO, true) // true lo hace Asíncrono
         xmlHttp.send() // al ser GET puede ir sin nada o con valor NULL
 
         xmlHttp.onload = function () {
