7379101b84862ff850ecf24d6ab3ee6ea793eacd
Pravos94 <alvaropr69@gmail.com>
2023-12-04 16:57:22 +0100

    img

--- AE-3_Angular_v2/src/app/listado-videojuegos/listado-videojuegos.component.ts
+++ AE-3_Angular_v2/src/app/listado-videojuegos/listado-videojuegos.component.ts
@@ -14,10 +14,10 @@ export class ListadoVideojuegosComponent implements OnInit {
   constructor() {
     this.datosJuegos = [];
   
-    this.datosJuegos.push(new Juego(1, "The Witcher 3: Wild Hunt", "CD Projekt Red", "/img/diablo1_1.jpg", '9 / 10'));
-    this.datosJuegos.push(new Juego(2, "The Legend of Zelda: Breath of the Wild", "Nintendo", "/img/diablo1_1.jpg", '10 / 10'));
-    this.datosJuegos.push(new Juego(3, "Red Dead Redemption 2", "Rockstar Games", "/img/diablo1_1.jpg", '9.5 / 10'));
-    this.datosJuegos.push(new Juego(4, "God of War", "Santa Monica Studio", "/img/diablo1_1.jpg", '9.7 / 10'));
+    this.datosJuegos.push(new Juego(1, "The Witcher 3: Wild Hunt", "CD Projekt Red", "/img/witcher3.jpg", '9 / 10'));
+    this.datosJuegos.push(new Juego(2, "The Legend of Zelda: Breath of the Wild", "Nintendo", "/img/zelda.jpg", '10 / 10'));
+    this.datosJuegos.push(new Juego(3, "Red Dead Redemption 2", "Rockstar Games", "/img/red.jpg", '9.5 / 10'));
+    this.datosJuegos.push(new Juego(4, "God of War", "Santa Monica Studio", "/img/war.jpg", '9.7 / 10'));
     this.datosJuegos.push(new Juego(5, "Diablo", "Atlus", "/img/diablo1_1.jpg", '8.7 / 10'));
     
   }
